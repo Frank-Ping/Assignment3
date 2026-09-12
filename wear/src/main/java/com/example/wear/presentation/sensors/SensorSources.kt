7db402cc85ae0107695df0e978558b3302a37d54
Frank-Ping <nzuowyhp@gmail.com>
2026-09-13 09:34:14 +1200

    Part B: Define accelerometer and heart rate source interfaces

--- wear/src/main/java/com/example/wear/presentation/sensors/SensorSources.kt
+++ wear/src/main/java/com/example/wear/presentation/sensors/SensorSources.kt
@@ -1,4 +1,23 @@
 package com.example.wear.presentation.sensors
 
-class SensorSources {
+import com.example.wear.presentation.data.AccelerometerRecord
+import com.example.wear.presentation.data.HeartRateRecord
+import com.example.wear.presentation.data.SensorStatus
+
+interface AccelerometerSource {
+    fun start(
+        onRecord: (AccelerometerRecord) -> Unit,
+        onStatusChanged: (SensorStatus) -> Unit
+    )
+
+    fun stop()
+}
+
+interface HeartRateSource {
+    fun start(
+        onRecord: (HeartRateRecord) -> Unit,
+        onStatusChanged: (SensorStatus) -> Unit
+    )
+
+    fun stop()
 }
