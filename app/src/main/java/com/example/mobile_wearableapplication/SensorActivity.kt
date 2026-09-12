fbb128a1a79b96f0f93c17b87eea6a1f3984e35d
Frank-Ping <nzuowyhp@gmail.com>
2026-09-12 21:19:22 +1200

    Part A: Modify the SensorActivity and set placeholder

--- app/src/main/java/com/example/mobile_wearableapplication/SensorActivity.kt
+++ app/src/main/java/com/example/mobile_wearableapplication/SensorActivity.kt
@@ -1,20 +1,101 @@
 package com.example.mobile_wearableapplication
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush.Companion.verticalGradient
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mobile_wearableapplication.ui.theme.MobileWearableApplicationTheme
 
-class SensorActivity : AppCompatActivity() {
+class SensorActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sensor)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        setContent {
+            MobileWearableApplicationTheme(darkTheme = true) {
+                SensorPage(
+                    onBack = { finish() }
+                )
+            }
         }
+    }
+}
+
+@Composable
+private fun SensorPage(onBack: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                verticalGradient(
+                    colors = listOf(
+                        Color(0xFF000000),
+                        Color(0xFF303030)
+                    )
+                )
+            )
+            .safeDrawingPadding()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(horizontal = 28.dp, vertical = 50.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(
+                space = 24.dp,
+                alignment = Alignment.CenterVertically
+            )
+        ) {
+            Text(
+                text = "Sensor Data",
+                color = Color.White,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.SemiBold,
+                textAlign = TextAlign.Center
+            )
+
+            Text(
+                text = "No session started",
+                modifier = Modifier.padding(bottom = 80.dp),
+                color = Color.Gray,
+                fontSize = 14.sp,
+                textAlign = TextAlign.Center
+            )
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SensorPagePreview() {
+    MobileWearableApplicationTheme(darkTheme = true) {
+        SensorPage(onBack = {})
     }
 }
